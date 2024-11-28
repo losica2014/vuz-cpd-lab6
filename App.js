18a502813505d061b2019c9db81a070ff78101de
Alexander Lozitskiy <losica@yandex.ru>
2024-11-29 00:19:41 +0300

    -

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StoreItem from './components/StoreItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <StoreItem name="Item 1" description="Description 1" price="Price 1" amount={10} setAmount={(a) => {}} />
     </View>
   );
 }
